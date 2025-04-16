124cdf1735fb519291d95a4df3a9c211428a119a
Lester Lee <contact@lester-lee.com>
2025-04-16 13:50:27 -0400

    Write /tracks endpoints

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 import express from "express";
 const app = express();
 export default app;
+
+import morgan from "morgan";
+
+import tracksRouter from "#api/tracks";
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/tracks", tracksRouter);
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    return res.status(400).send(err.message);
+  }
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong.");
+});
--- db/queries/tracks.js
+++ db/queries/tracks.js
@@ -13,3 +13,24 @@ export async function createTrack(name, durationMs) {
   } = await db.query(sql, [name, durationMs]);
   return track;
 }
+
+export async function getTracks() {
+  const sql = `
+  SELECT *
+  FROM tracks
+  `;
+  const { rows: tracks } = await db.query(sql);
+  return tracks;
+}
+
+export async function getTrackById(id) {
+  const sql = `
+  SELECT *
+  FROM tracks
+  WHERE id = $1
+  `;
+  const {
+    rows: [track],
+  } = await db.query(sql, [id]);
+  return track;
+}
