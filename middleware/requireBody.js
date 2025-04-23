df500ba98c7bfd0f4ee78e5851f98fd56634a163
Lester Lee <contact@lester-lee.com>
2025-04-23 12:32:07 -0400

    Fix requireBody

--- middleware/requireBody.js
+++ middleware/requireBody.js
@@ -3,7 +3,7 @@ export default function requireBody(fields) {
   return (req, res, next) => {
     if (!req.body) return res.status(400).send("Request body is required.");
 
-    const missing = fields.filter((field) => !req.body[field]);
+    const missing = fields.filter((field) => !(field in req.body));
     if (missing.length > 0)
       return res.status(400).send(`Missing fields: ${missing.join(", ")}`);
 
