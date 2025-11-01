85a4b7f709cbbe654a999be2d98ac1e9f6bf4285
Kevin MacDonald <iamkevinmac@gmail.com>
2025-11-01 13:11:56 -0700

    added authentication and authorization to playlist routes

--- api/playlists.js
+++ api/playlists.js
@@ -1,28 +1,33 @@
 import express from "express";
 const router = express.Router();
-export default router;
 
 import {
   createPlaylist,
   getPlaylistById,
-  getPlaylists,
+  getPlaylistsByUserId,
 } from "#db/queries/playlists";
+
 import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 import { getTracksByPlaylistId } from "#db/queries/tracks";
 
+import getUserFromToken from "#middleware/getUserFromToken";
+import requireUser from "#middleware/requireUser";
+import requireBody from "#middleware/requireBody";
+
+router.use(getUserFromToken);
+router.use(requireUser);
+//router.use here is applying middleware to all routes in the playlists router (extracts user from token and check if they are logged in)
+
 router.get("/", async (req, res) => {
-  const playlists = await getPlaylists();
+  const playlists = await getPlaylistsByUserId(req.user.id);
   res.send(playlists);
 });
 
-router.post("/", async (req, res) => {
-  if (!req.body) return res.status(400).send("Request body is required.");
-
+router.post("/", requireBody(["name", "description"]), async (req, res) => {
   const { name, description } = req.body;
-  if (!name || !description)
-    return res.status(400).send("Request body requires: name, description");
 
-  const playlist = await createPlaylist(name, description);
+  const playlist = await createPlaylist(name, description, req.user.id);
+
   res.status(201).send(playlist);
 });
 
@@ -30,6 +35,12 @@ router.param("id", async (req, res, next, id) => {
   const playlist = await getPlaylistById(id);
   if (!playlist) return res.status(404).send("Playlist not found.");
 
+  //check if user owns playlist
+  if (playlist.user_id !== req.user.id) {
+    return res
+      .status(403)
+      .send("You don't have permission to access this playlist");
+  }
   req.playlist = playlist;
   next();
 });
@@ -43,12 +54,12 @@ router.get("/:id/tracks", async (req, res) => {
   res.send(tracks);
 });
 
-router.post("/:id/tracks", async (req, res) => {
-  if (!req.body) return res.status(400).send("Request body is required.");
-
+router.post("/:id/tracks", requireBody(["trackId"]), async (req, res) => {
   const { trackId } = req.body;
-  if (!trackId) return res.status(400).send("Request body requires: trackId");
 
   const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
+
   res.status(201).send(playlistTrack);
 });
+
+export default router;
